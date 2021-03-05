4ff79e9dabe85f8d8f6ac646f0e9ea7fc21dbdb1
Anton <rostov.develop.ua@gmail.com>
2021-03-05 03:52:11 +0000

    Add routes to previous challenge & add ESLint

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+app.get('/rockpaperscissor', (req, res) => {
+  res.sendFile('./views/rockpaperscissor.html', { root: __dirname });
+});
