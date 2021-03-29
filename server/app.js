44e7882f44ca56bd2bce9e2928b1222ae0dd49b3
Anton <rostov.develop.ua@gmail.com>
2021-03-29 12:30:08 +0000

    folder restructure, babel.js, & edit readme

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
-const routes = require('./routes/routes');
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import routes from './routes/routes';
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 app.use(morgan('tiny'));
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const controller = require('../controllers/controller');
+import express from 'express';
+import controller from '../controllers/controller';
 const router = express.Router();
 router.get('/', controller.home_index);
 router.get('/index', (req, res) => {
@@ -10,4 +10,4 @@ router.get('/home', (req, res) => {
 });
 router.get('/rockpaperscissor', controller.rps_index);
 router.post('/rockpaperscissor', controller.rps_history);
-module.exports = router;
+export default router;
