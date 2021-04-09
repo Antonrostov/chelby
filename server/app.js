1be942ff4756107287b0762730cf159ab6643536
Anton <rostov.develop.ua@gmail.com>
2021-04-09 14:02:15 +0000

    Signup & Login page

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import routes from './routes/routes';
+import authRoutes from './routes/authRoutes';
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
@@ -14,6 +15,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use('/auth', authRoutes);
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
