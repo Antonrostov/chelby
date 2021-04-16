cccc10dc3959cf075dbeb40b74d61293eebd1ab2
Anton <rostov.develop.ua@gmail.com>
2021-04-16 09:51:42 +0000

    login form and data validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { userGames, userGameBiodata } from '../models';
 import signupValidation from '../middlewares/validation/signupValidation';
+import loginValidation from '../middlewares/validation/loginValidation';
 class authController {
   static getSignup = (req, res) => {
     res.render('signup', { title: 'Sign Up', validateError: '' });
@@ -19,10 +20,8 @@ class authController {
       });
       if (error) return res.render('signup', { title: 'Sign Up', validateError: `${error.details[0].message}` });
       const emailExist = await userGames.findOne({ where: { email } });
-      const usernameExist = await userGames.findOne({ where: { username } });
-      console.log(emailExist);
-      console.log(usernameExist);
       if (emailExist) return res.render('signup', { title: 'Sign Up', validateError: 'Email is already signed up.' });
+      const usernameExist = await userGames.findOne({ where: { username } });
       if (usernameExist) return res.render('signup', { title: 'Sign Up', validateError: 'Username is already taken.' });
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
       await userGames.create({
@@ -35,14 +34,27 @@ class authController {
           name: req.body.name,
         }))
         .catch((e) => console.log(e));
-      return res.status(201).redirect('/auth/login');
+      return res.redirect('/auth/login');
     } catch {
-      return res.status(200).redirect('/auth/signup');
+      return res.redirect('/auth/signup');
     }
   };
   static getLogin = (req, res) => {
-    res.render('login', { title: 'Login' });
+    res.render('login', { title: 'Login', validateError: '' });
   };
-  static postLogin = (req, res) => { };
+  static postLogin = async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const { error } = await loginValidation.validate({ username, password });
+      if (error) return res.render('login', { title: 'Login', validateError: `${error.details[0].message}` });
+      const validUsername = await userGames.findOne({ where: { username } });
+      if (!validUsername) return res.render('login', { title: 'Login', validateError: 'Username is wrong.' });
+      const validPassword = await bcrypt.compare(password, validUsername.password) || validUsername.password;
+      if (!validPassword) return res.render('login', { title: 'Login', validateError: 'Password is wrong.' });
+      return res.redirect('/');
+    } catch {
+      return res.redirect('/auth/login');
+    }
+  };
 }
 export default authController;
