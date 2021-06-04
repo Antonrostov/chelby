8bec16a26f085b64be350dadf7a4976ec2ebadfe
Anton <rostov.develop.ua@gmail.com>
2021-06-04 23:20:52 +0000

    [KI] routes

--- server/controllers/APIController/GameHistoryAPIController.js
+++ server/controllers/APIController/GameHistoryAPIController.js
@@ -1,5 +1,33 @@
 import { userGameHistories } from '../../models';
-class GameHistoryAPIController {
+class gameAPIController {
+  static getGameHistory = async (req, res) => {
+    try {
+      const history = await userGameHistories.findAll({
+        attributes: ['historyId', 'timestamps', 'player_choice', 'comp_choice', 'result'],
+        where: { userId: req.session.userId },
+        order: [['timestamps', 'DESC']],
+      })
+        .catch((e) => console.log(e));
+      return res.render('game_history', { title: 'Game History', username: req.session.username, history });
+    } catch {
+      return res.render('game_history', { title: 'Game History', username: req.session.username, history: '' });
+    }
+  };
+  static postGameHistory = async (req, res) => {
+    try {
+      const { player_choice, comp_choice, result } = req.body;
+      await userGameHistories.create({
+        timestamps: new Date().toISOString(),
+        userId: req.session.userId,
+        player_choice,
+        comp_choice,
+        result,
+      }).catch((e) => console.log(e));
+      return res.status(201);
+    } catch {
+      return res.redirect('/game');
+    }
+  };
   static deleteGameHistory = async (req, res) => {
     try {
       const { historyId } = req.body;
@@ -11,4 +39,4 @@ class GameHistoryAPIController {
     }
   };
 }
-export default GameHistoryAPIController;
+export default gameAPIController;
---
+++ server/routes/apiRoutes/v1/userAPIRoute.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import UserAPIController from '../../../controllers/APIController/UserAPIController';
+const router = express.Router();
+export default router;
