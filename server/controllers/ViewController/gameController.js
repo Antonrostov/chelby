a3ee0a87c1247c2c080c9ca7f5279c928346d41d
Anton <rostov.develop.ua@gmail.com>
2021-06-06 11:38:00 +0000

    [KI] create api for get history

--- server/controllers/ViewController/gameController.js
+++ server/controllers/ViewController/gameController.js
@@ -1,20 +1,20 @@
 import { userGameHistories } from '../../models';
+import fetch from 'node-fetch'
 class gameController {
   static rpsIndex = (req, res) => {
     res.render('rockpaperscissor', { title: 'Rock Paper Scissor', username: req.session.username });
   };
   static getGameHistory = async (req, res) => {
-    try {
-      const history = await userGameHistories.findAll({
-        attributes: ['historyId', 'timestamps', 'player_choice', 'comp_choice', 'result'],
-        where: { userId: req.session.userId },
-        order: [['timestamps', 'DESC']],
+    await fetch(`http:
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === 200) {
+          return res.render('game_history', { title: 'Game History', username: req.session.username, history: data.history });
+        } else {
+          return res.render('game_history', { title: 'Game History', username: req.session.username, history: '' });
+        }
       })
-        .catch((e) => console.log(e));
-      return res.render('game_history', { title: 'Game History', username: req.session.username, history });
-    } catch {
-      return res.render('game_history', { title: 'Game History', username: req.session.username, history: '' });
-    }
+      .catch((e) => console.log(e));
   };
   static postGameHistory = async (req, res) => {
     try {
--- server/routes/apiRoutes/v1/gameAPIRoute.js
+++ server/routes/apiRoutes/v1/gameAPIRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import gameHistoryAPIController from '../../../controllers/APIController/GameHistoryAPIController';
 const router = express.Router();
-router.get('/history', gameHistoryAPIController.getGameHistory);
+router.get('/history/:id', gameHistoryAPIController.getGameHistory);
 router.post('/history', gameHistoryAPIController.postGameHistory);
 router.delete('/history', gameHistoryAPIController.deleteGameHistory);
 export default router;
