9c0172e3380273418e59e829517901748fd1e241
Anton <rostov.develop.ua@gmail.com>
2021-07-05 20:33:05 +0000

    separate auth view & auth API controller

--- server/controllers/ViewController/authController.js
+++ server/controllers/ViewController/authController.js
@@ -1,5 +1,4 @@
-import bcrypt from 'bcrypt';
-import { userGames, userGameBiodata } from '../../models';
+import fetch from 'node-fetch';
 import checkUserId from '../../middlewares/authentication/checkUserId';
 class authController {
   static getSignup = (req, res) => {
@@ -8,27 +7,15 @@ class authController {
   };
   static postSignup = async (req, res) => {
     const login = checkUserId(req.session);
-    try {
-      const { email, username } = req.body;
-      const emailExist = await userGames.findOne({ where: { email } });
-      if (emailExist) return res.render('signup', { title: 'Sign Up', login, validateError: 'Email is already taken.' });
-      const usernameExist = await userGames.findOne({ where: { username } });
-      if (usernameExist) return res.render('signup', { title: 'Sign Up', login, validateError: 'Username is already taken.' });
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      await userGames.create({
-        email: req.body.email,
-        username: req.body.username,
-        password: hashedPassword,
+    await fetch(`http:
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 201) {
+          return res.redirect('/auth/login');
+        }
+        return res.render('signup', { title: 'signup', login, validateError: data.message });
       })
-        .then((data) => userGameBiodata.create({
-          userId: data.userId,
-          name: req.body.name,
-        }))
-        .catch((e) => console.log(e));
-      return res.redirect('/auth/login');
-    } catch {
-      return res.redirect('/auth/signup', { login });
-    }
+      .catch((e) => console.log(e));
   };
   static getLogin = (req, res) => {
     const login = checkUserId(req.session);
@@ -36,38 +23,32 @@ class authController {
   };
   static postLogin = async (req, res) => {
     const login = checkUserId(req.session);
-    try {
-      const { username, password } = req.body;
-      const validUsername = await userGames.findOne({ where: { username } });
-      if (!validUsername) {
-        return res.render('login', {
-          title: 'Login', login, validateError: 'Username is wrong.',
-        });
-      }
-      const validPassword = await bcrypt.compare(password, validUsername.password);
-      if (!validPassword) {
-        return res.render('login', {
-          title: 'Login', login, validateError: 'Password is wrong.',
-        });
-      }
-      if (validUsername) {
-        req.session.userId = validUsername.userId;
-        req.session.username = validUsername.username;
-      }
-      return res.render('index', { title: 'Home', login: true, username: req.session.username });
-    } catch {
-      return res.redirect('/auth/login');
-    }
+    await fetch(`http:
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 200) {
+          if (data.userId && data.username) {
+            req.session.userId = data.userId;
+            req.session.username = data.username;
+          }
+          return res.render('index', { title: 'Home', login: true, username: req.session.username });
+        }
+        return res.render('login', { title: 'login', login, validateError: data.message });
+      })
+      .catch((e) => console.log(e));
   };
-  static logout = (req, res) => {
-    const login = checkUserId(req.session);
-    req.session.destroy((err) => {
-      if (err) {
-        return res.render('index', { title: 'Home', login, username: '' });
-      }
-      res.clearCookie(process.env.SESSION_NAME);
-      return res.redirect('/auth/login');
-    });
+  static logout = async (req, res) => {
+    await fetch(`http:
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 302) {
+          req.session.destroy();
+          res.clearCookie(process.env.SESSION_NAME);
+          return res.render('login', { title: 'Login', login: false, validateError: '' });
+        }
+        return res.redirect('/profile');
+      })
+      .catch((e) => console.log(e));
   }
 }
 export default authController;
