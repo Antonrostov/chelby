e7d0f51b84842947c31310f48cfd5add397774aa
Anton <rostov.develop.ua@gmail.com>
2021-07-09 13:39:47 +0000

    simplify import middlewares

---
+++ server/middlewares/authentication/index.js
@@ -0,0 +1,8 @@
+import blockAuthenticated from './blockAuthenticated';
+import blockUnauthenticated from './blockUnauthenticated';
+import checkUserId from './checkUserId';
+export default {
+  blockAuthenticated,
+  blockUnauthenticated,
+  checkUserId,
+};
