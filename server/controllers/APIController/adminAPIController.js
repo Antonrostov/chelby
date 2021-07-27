b42431a76f2d8b8964c29e15618cc6f4186ca67a
Anton <rostov.develop.ua@gmail.com>
2021-07-27 11:11:18 +0000

    demote admin except 'superadmin'

--- server/controllers/APIController/adminAPIController.js
+++ server/controllers/APIController/adminAPIController.js
@@ -12,15 +12,30 @@ class adminAPIController {
       return res.status(403).json({ status: 403, message: 'Forbidden.' });
     }
   };
-  static makeAdmin = async (req, res) => {
+  static promoteAdmin = async (req, res) => {
     try {
       const { userId } = req.body;
-      console.log(userId);
       await userGames.findOne({
         where: { userId },
       })
         .then((user) => {
           if (userId) { user.update({ isAdmin: true }); }
+        })
+        .then(() => res.redirect('/admin/userlist'))
+        .catch((e) => console.log(e));
+      return res.status(201);
+    } catch {
+      return res.status(403);
+    }
+  };
+  static demoteAdmin = async (req, res) => {
+    try {
+      const { userId } = req.body;
+      await userGames.findOne({
+        where: { userId },
+      })
+        .then((user) => {
+          if (userId) { user.update({ isAdmin: false }); }
         })
         .then(() => res.redirect('/admin/userlist'))
         .catch((e) => console.log(e));
--- server/routes/apiRoutes/v1/adminAPIRoute.js
+++ server/routes/apiRoutes/v1/adminAPIRoute.js
@@ -2,5 +2,6 @@ import express from 'express';
 import adminAPIController from '../../../controllers/APIController/adminAPIController';
 const router = express.Router();
 router.get('/userlist/:id', adminAPIController.getUserlist);
-router.patch('/makeAdmin', adminAPIController.makeAdmin);
+router.patch('/promoteAdmin', adminAPIController.promoteAdmin);
+router.patch('/demoteAdmin', adminAPIController.demoteAdmin);
 export default router;
