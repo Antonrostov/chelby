7800abc54d9f9dea10e9d00c640adfa38e278836
Anton <rostov.develop.ua@gmail.com>
2021-08-16 03:52:41 +0000

    getRoom API

--- server/routes/apiRoutes/v1/gameAPIRoute.js
+++ server/routes/apiRoutes/v1/gameAPIRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import gameAPIController from '../../../controllers/APIController/gameAPIController';
 const router = express.Router();
+router.get('/room', gameAPIController.getRoom);
 router.get('/room/create', gameAPIController.createRoom);
 router.get('/history/:id', gameAPIController.getGameHistory);
 router.post('/history/:id', gameAPIController.postGameHistory);
